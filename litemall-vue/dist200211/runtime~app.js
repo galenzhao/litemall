6bf2449b83ac7b5eb4d6a5a35996c33e44280cda
galenzhao <galenzha0@vip.qq.com>
2020-02-12 11:59:13 +0800

    mv -

--- litemall-vue/dist200211/runtime~app.js
+++ litemall-vue/dist200211/runtime~app.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1240d82ddbe01162d49d";
+/******/ 	var hotCurrentHash = "a368d34cbde70ae2a2e6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
